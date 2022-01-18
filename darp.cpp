83d97a3b24e1a024a0e1a1fd2f5fe08443cbc32c
mavalu22 <matheusnardi@gmail.com>
2022-01-17 21:24:45 -0300

    leitura e structs

--- darp.cpp
+++ darp.cpp
@@ -1,7 +1,90 @@
-
-
+#include <iostream>
+#include <time.h>
+#include <cstring>
+#include <memory.h>
+#include <math.h>
 #include "darp.hpp"
 
-int main(int argc, char *argv[])
+using namespace std;
+
+#define MAX(X, Y) ((X > Y) ? X : Y)
+
+int main(int argc, const char *argv[])
 {
+
+    string instancia = "";
+
+    lerDados();
+    printMatriz(matrizTempDeslocamento);
+}
+
+void lerDados()
+{
+
+    FILE *f = fopen("darp1.txt", "r");
+
+    fscanf(f, "%d %d %d %d %d", &lixo, &requisicoes, &duracaoMaxRota, &tempMaxViagem, &tempMaxEspera);
+
+    requisicoes = (requisicoes * 2) + 2;
+
+    for (int i = 0; i < VEICULOS; i++)
+    {
+        fscanf(f, "%d", &capacidadeVeic[i]);
+    }
+
+    for (int i = 0; i < requisicoes; i++)
+    {
+        fscanf(f, "%d", &numAssentosLocal[i]);
+    }
+
+    for (int i = 0; i < requisicoes; i++)
+    {
+        fscanf(f, "%d", &tempServico[i]);
+    }
+
+    for (int i = 0; i < requisicoes; i++)
+    {
+        fscanf(f, "%d", &inicioJanelaTemp[i]);
+    }
+
+    for (int i = 0; i < requisicoes; i++)
+    {
+        fscanf(f, "%d", &fimJanelaTemp[i]);
+    }
+    for (int i = 0; i < requisicoes; i++)
+    {
+        for (int j = 0; j < requisicoes; j++)
+        {
+            fscanf(f, "%d", &matrizTempDeslocamento[i][j]);
+            // printf("%d %d\n", i, j);
+        }
+    }
+    fclose(f);
+}
+
+void printVetor(int vet[VEICULOS])
+{
+    for (int i = 0; i < VEICULOS; i++)
+    {
+        printf("%d \t", vet[i]);
+    }
+}
+
+void printOutroVetor(int vet[MAX_REQ])
+{
+    for (int i = 0; i < requisicoes; i++)
+    {
+        printf("%d \t", vet[i]);
+    }
+}
+
+void printMatriz(int matriz[MAX_REQ][MAX_REQ])
+{
+    for (int i = 0; i < requisicoes; i++)
+    {
+        for (int j = 0; j < requisicoes; j++)
+        {
+            printf("%d \t", matriz[i][j]);
+        }
+    }
 }
--- darp.hpp
+++ darp.hpp
@@ -1,3 +1,6 @@
+#ifndef DARP_HPP_INCLUDED
+#define DARP_HPP_INCLUDED
+
 #define MAX_VIA 1440
 #define MAX_ESP 1440
 #define CARROS 6
@@ -9,10 +12,62 @@ typedef struct input
     int req;
     int durMaxRot;
     int vetNumAss[CARROS];
-    int vetNumAssLoc[locais];
-    int vetTempServ[];
-    int vetIniTemp[];
-    int vetTermTemp[];
-    int matTemp[][];
+    // int vetNumAssLoc[locais];
+    // int vetTempServ[];
+    // int vetIniTemp[];
+    // int vetTermTemp[];
+    // int matTemp[][];
     int funObj;
 } input;
+
+typedef struct req
+{
+    int saida;
+    int chegada;
+} req;
+
+typedef struct veiculo
+{
+    int id;
+    int vaga;
+    // int requisicoes[];
+    int horario;
+    int durRota;
+    int durViagem;
+    int tempEspera;
+    int violAssentos;
+    int violDurRota;
+    int violDurViagem;
+    int violTempEsp;
+    int violSaidaChegada;
+
+} veiculo;
+
+typedef struct passageiro
+{
+    int tempoViagem;
+    int tempoEspera;
+} passageiro;
+
+#include <string>
+#define VEICULOS 6
+#define MAX_REQ 50
+
+int requisicoes, duracaoMaxRota, tempMaxViagem, tempMaxEspera, lixo;
+
+int capacidadeVeic[VEICULOS];
+int numAssentosLocal[MAX_REQ];
+int tempServico[MAX_REQ];
+int inicioJanelaTemp[MAX_REQ];
+int fimJanelaTemp[MAX_REQ];
+int matrizTempDeslocamento[MAX_REQ][MAX_REQ];
+
+void lerDados();
+
+void printVetor(int vet[MAX_REQ]);
+
+void printOutroVetor(int vet[VEICULOS]);
+
+void printMatriz(int matriz[MAX_REQ][MAX_REQ]);
+
+#endif
